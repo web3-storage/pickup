f649119ad179bac812d6a9c8bc3924a05abb011a
Oli Evans <oli@protocol.ai>
2022-10-19 15:41:29 +0100

    chore: add aws tagging
    License: MIT
    Signed-off-by: Oli Evans <oli@protocol.ai>

--- stacks/index.ts
+++ stacks/index.ts
@@ -1,3 +1,4 @@
+import { Tags } from 'aws-cdk-lib'
 import { PickupStack } from './PickupStack'
 import { BasicApiStack } from './BasicApiStack'
 import { App } from '@serverless-stack/resources'
@@ -12,4 +13,11 @@ export default function (app: App): void {
   })
   app.stack(BasicApiStack)
   app.stack(PickupStack)
+
+  // tags let us discover all the aws resource costs incurred by this app
+  // see: https://docs.sst.dev/advanced/tagging-resources
+  Tags.of(app).add('Project', 'pickup')
+  Tags.of(app).add('Repository', 'https://github.com/web3-storage/pickup')
+  Tags.of(app).add('Environment', `${app.stage}`)
+  Tags.of(app).add('ManagedBy', 'SST')
 }
