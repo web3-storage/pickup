92c3b062aa3899d945b43ed3ec8172a198a89089
Oli Evans <oli@protocol.ai>
2022-11-08 15:25:22 +0000

    feat: abort requests after timeout
    - Adds a `timeoutMs` param to all ipfs requests.
    - fetchCar may take a long time, so the timeout is treated as a rolling window. We restart the countdown when a chunk of data comes in.
    
    License: MIT
    Signed-off-by: Oli Evans <oli@protocol.ai>

--- pickup/lib/ipfs.js
+++ pickup/lib/ipfs.js
@@ -1,42 +1,56 @@
+import { compose } from 'node:stream'
 import { CID } from 'multiformats/cid'
 import { Multiaddr } from 'multiaddr'
+import debounce from 'debounce'
 import fetch from 'node-fetch'
 
-export async function fetchCar (cid, ipfsApiUrl) {
+export async function fetchCar (cid, ipfsApiUrl, timeoutMs = 30000) {
   if (!isCID(cid)) {
     throw new Error({ message: `Invalid CID: ${cid}` })
   }
   const url = new URL(`/api/v0/dag/export?arg=${cid}`, ipfsApiUrl)
-  const res = await fetch(url, { method: 'POST' })
+  const ctl = new AbortController()
+  const startCountdown = debounce(() => ctl.abort(), timeoutMs)
+  startCountdown()
+  const res = await fetch(url, { method: 'POST', signal: ctl.signal })
   if (!res.ok) {
     throw new Error(`${res.status} ${res.statusText} ${url}`)
   }
-  return res.body
+  async function* restartCountdown (source) {
+    for await (const chunk of source) {
+      startCountdown()
+      yield chunk
+    }
+  }
+  return compose(res.body, restartCountdown)
 }
 
-export async function connectTo (origins = [], ipfsApiUrl) {
+export async function connectTo (origins = [], ipfsApiUrl, timeoutMs = 10000) {
   for (const addr of origins.filter(isMultiaddr)) {
     const url = new URL(`/api/v0/swarm/connect?arg=${addr}`, ipfsApiUrl)
-    const res = await fetch(url, { method: 'POST' })
+    const signal = AbortSignal.timeout(timeoutMs)
+    const res = await fetch(url, { method: 'POST', signal })
     if (!res.ok) {
       console.log(`Error connecting to ${addr} - got: ${res.status} ${res.statusText}`)
     }
   }
 }
 
-export async function disconnect (origins = [], ipfsApiUrl) {
+export async function disconnect (origins = [], ipfsApiUrl, timeoutMs = 10000) {
   for (const addr of origins.filter(isMultiaddr)) {
     const url = new URL(`/api/v0/swarm/disconnect?arg=${addr}`, ipfsApiUrl)
-    const res = await fetch(url, { method: 'POST' })
+    const signal = AbortSignal.timeout(timeoutMs)
+    const res = await fetch(url, { method: 'POST', signal })
     if (!res.ok) {
       console.log(`Error disconnecting from ${addr} - got: ${res.status} ${res.statusText}`)
     }
   }
 }
 
-export async function waitForGC (ipfsApiUrl) {
+export async function waitForGC (ipfsApiUrl, timeoutMs = 60000) {
   const url = new URL('/api/v0/repo/gc?silent=true', ipfsApiUrl)
-  const res = await fetch(url, { method: 'POST' })
+  const signal = AbortSignal.timeout(timeoutMs)
+  const res = await fetch(url, { method: 'POST', signal })
   if (!res.ok) {
     console.log(`Error GCing - got: ${res.status} ${res.statusText}`)
   }
@@ -57,10 +71,11 @@ export function isCID (str) {
   return Boolean(CID.parse(str))
 }
 
-export async function testIpfsApi (ipfsApiUrl) {
+export async function testIpfsApi (ipfsApiUrl, timeoutMs = 10000) {
   const url = new URL('/api/v0/id', ipfsApiUrl)
+  const signal = AbortSignal.timeout(timeoutMs)
   try {
-    const res = await fetch(url, { method: 'POST' })
+    const res = await fetch(url, { method: 'POST', signal })
     if (!res.ok) {
       if (res.status === 404) {
         throw new Error('IPFS API returned 404. IPFS_API_URL must be the RPC API port (:5001) rather than the ipfsApiUrl port (:8080)')
