63f18216509eaa58091992cff1805d3d274cd834
Oli Evans <oli@protocol.ai>
2022-11-10 15:15:53 +0000

    docs: add link to kubo config docs
    License: MIT
    Signed-off-by: Oli Evans <oli@protocol.ai>

--- pickup/ipfs/ipfs-config.sh
+++ pickup/ipfs/ipfs-config.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-## The shell in the go-ipfs container is busybox, so a version of ash
+## The shell in the kubo container is busybox, so a version of ash
 ## Shellcheck might warn on things POSIX sh cant do, but ash can
 ## In Shellcheck, ash is an alias for dash, but busybox ash can do more than dash 
 ## https://github.com/koalaman/shellcheck/blob/master/src/ShellCheck/Data.hs#L134
+
+# kubo config docs: https://github.com/ipfs/kubo/blob/master/docs/config.md
 
 # dont add provider records to the dht... e-ipfs will do that.
 ipfs config --json Experimental.StrategicProviding true
