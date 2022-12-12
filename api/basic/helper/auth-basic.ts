af2543c6020525ed9cb0ee40fe9fee1e27f3a9b7
Francisco Cardoso <francardoso@outlook.com>
2022-12-12 17:10:22 -0300

    ts errors

--- api/basic/helper/auth-basic.ts
+++ api/basic/helper/auth-basic.ts
@@ -1,7 +1,7 @@
 import { Config } from '@serverless-stack/node/config'
 import { Response } from '../schema.js'
 
-export function doAuth(authorizationHeader): Response | undefined {
+export function doAuth(authorizationHeader: string): Response | undefined {
   if (authorizationHeader !== `Basic ${getValidCredentials()}`) {
     return {
       statusCode: 401,
@@ -14,6 +14,7 @@ async function getValidCredentials() {
   let validCredentials = process.env.CLUSTER_BASIC_AUTH_TOKEN
   if (!validCredentials) {
     // If not set as environment variable...
+    // @ts-ignore:next-line
     validCredentials = Config.AUTH_TOKEN //... Get it from AWS SSM parameter store
   }
   return validCredentials
