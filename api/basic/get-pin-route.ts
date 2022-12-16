8590cd9a2552a1cafdce9468145b270053a0fef4
Davide Fiorello <davide@codeflyer.com>
2022-12-16 13:28:22 +0100

    feat: getPin router

---
+++ api/basic/get-pin-route.ts
@@ -0,0 +1,95 @@
+import { APIGatewayProxyEventV2 } from 'aws-lambda'
+import { ClusterStatusResponse, Response } from './schema.js'
+import { CID } from 'multiformats/cid'
+import fetch from 'node-fetch'
+
+/**
+ * AWS API Gateway handler for GET /pins/${cid}
+ * Collect the params and delegate to getPin to do the work
+ *
+ * We provide responses in Payload format v2.0
+ * see: https://docs.aws.amazon.com/apigateway/latest/developerguide/http-api-develop-integrations-lambda.html#http-api-develop-integrations-lambda.proxy-format
+ */
+export async function handler (event: APIGatewayProxyEventV2): Promise<Response> {
+  const {
+    CLUSTER_BASIC_AUTH_TOKEN: token = '',
+    INDEXER_ENDPOINT: indexerEndpoint = '',
+    PICKUP_ENDPOINT: pickupEndpoint = ''
+  } = process.env
+
+  if (event.headers.authorization !== `Basic ${token}`) {
+    return { statusCode: 401, body: JSON.stringify({ error: { reason: 'UNAUTHORIZED' } }) }
+  }
+
+  const cid = event.pathParameters?.cid ?? ''
+
+  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+  if (!cid) {
+    return { statusCode: 400, body: JSON.stringify({ error: { reason: 'BAD_REQUEST', details: 'CID not found in path' } }) }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+  if (!indexerEndpoint) {
+    return { statusCode: 500, body: JSON.stringify({ error: { reason: 'INTERNAL_SERVER_ERROR', details: 'INDEXER_ENDPOINT not defined' } }) }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+  if (!pickupEndpoint) {
+    return { statusCode: 500, body: JSON.stringify({ error: { reason: 'INTERNAL_SERVER_ERROR', details: 'PICKUP_ENDPOINT not defined' } }) }
+  }
+
+  if (!isCID(cid)) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: { reason: 'BAD_REQUEST', details: `${cid} is not a valid CID` } })
+    }
+  }
+
+  try {
+    const pickupResponse = await fetchGetPin({ cid, endpoint: pickupEndpoint, isInternal: true, token })
+
+    if (pickupResponse.statusCode === 200 && (Object.values(pickupResponse.body?.peer_map).filter(pin => pin.status !== 'unpinned').length > 0)) {
+      return { ...pickupResponse, body: JSON.stringify(pickupResponse.body) }
+    }
+
+    const indexerResponse = await fetchGetPin({ cid, endpoint: indexerEndpoint, token })
+
+    return { ...indexerResponse, body: JSON.stringify(indexerResponse.body) }
+  } catch (error) {
+    console.log(error)
+    return { statusCode: 500, body: JSON.stringify({ error: { reason: 'INTERNAL_SERVER_ERROR' } }) }
+  }
+}
+
+// Load balancing extensions
+interface GetPinResult {
+  statusCode: number
+  body: ClusterStatusResponse
+}
+
+interface FetchGetPinParams {
+  cid: string
+  endpoint: string
+  token: string
+  isInternal?: boolean
+}
+
+async function fetchGetPin ({
+  cid,
+  endpoint,
+  token,
+  isInternal = false
+}: FetchGetPinParams): Promise<GetPinResult> {
+  const myURL = new URL(`${isInternal ? '/internal' : ''}/pins/${cid}`, endpoint)
+  const result = await fetch(myURL.href, { method: 'GET', headers: { Authorization: `Basic ${token}` } })
+
+  return { statusCode: result.status, body: (await result.json()) as ClusterStatusResponse }
+}
+
+export function isCID (str = ''): boolean {
+  try {
+    return Boolean(CID.parse(str))
+  } catch (err) {
+    return false
+  }
+}
---
+++ api/test/get-pin-route-handler.test.js
@@ -0,0 +1,214 @@
+import test from 'ava'
+import nock from 'nock'
+
+import { handler } from '../basic/get-pin-route.js'
+
+import responseGetPinUnpinned from './__data/response-get-pin-unpinned.js'
+import responseGetPinPinned from './__data/response-get-pin-pinned.js'
+import responseGetPinQueued from './__data/response-get-pin-queued.js'
+
+test.before(async t => {
+  t.context.indexerEndpoint = 'http://indexer.loc'
+  t.context.pickupEndpoint = 'http://pickup.loc'
+})
+
+test('get pin route handler basic auth fail', async t => {
+  process.env.CLUSTER_BASIC_AUTH_TOKEN = 'YES'
+
+  const event = {
+    headers: {
+      authorization: 'nope'
+    },
+    pathParameters: {
+      cid: 'bafybeiczsscdsbs7ffqz55asqdf3smv6klcw3gofszvwlyarci47bgf354'
+    }
+  }
+  const response = await handler(event)
+  t.is(response.statusCode, 401)
+  t.true(typeof response.body === 'string')
+  t.deepEqual(JSON.parse(response.body), { error: { reason: 'UNAUTHORIZED' } })
+})
+
+test('get pin route handler without cid', async t => {
+  process.env.CLUSTER_BASIC_AUTH_TOKEN = 'YES'
+  process.env.INDEXER_ENDPOINT = t.context.indexerEndpoint
+  process.env.PICKUP_ENDPOINT = t.context.pickupEndpoint
+
+  const cid = ''
+
+  const event = {
+    headers: {
+      authorization: `Basic ${process.env.CLUSTER_BASIC_AUTH_TOKEN}`
+    },
+    pathParameters: {
+      cid
+    }
+  }
+  const response = await handler(event)
+
+  t.deepEqual(response, {
+    statusCode: 400,
+    body: '{"error":{"reason":"BAD_REQUEST","details":"CID not found in path"}}'
+  })
+})
+
+test('get pin route handler with invalid cid', async t => {
+  process.env.CLUSTER_BASIC_AUTH_TOKEN = 'YES'
+  process.env.INDEXER_ENDPOINT = t.context.indexerEndpoint
+  process.env.PICKUP_ENDPOINT = t.context.pickupEndpoint
+
+  const cid = '123123123'
+
+  const event = {
+    headers: {
+      authorization: `Basic ${process.env.CLUSTER_BASIC_AUTH_TOKEN}`
+    },
+    pathParameters: {
+      cid
+    }
+  }
+  const response = await handler(event)
+
+  t.deepEqual(response, {
+    statusCode: 400,
+    body: '{"error":{"reason":"BAD_REQUEST","details":"123123123 is not a valid CID"}}'
+  })
+})
+
+test('get pin route handler with invalid indexerEndpoint', async t => {
+  process.env.CLUSTER_BASIC_AUTH_TOKEN = 'YES'
+  process.env.INDEXER_ENDPOINT = t.context.indexerEndpoint
+  process.env.PICKUP_ENDPOINT = ''
+
+  const cid = 'QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn'
+  const event = {
+    headers: {
+      authorization: `Basic ${process.env.CLUSTER_BASIC_AUTH_TOKEN}`
+    },
+    pathParameters: {
+      cid
+    }
+  }
+  const response = await handler(event)
+
+  t.deepEqual(response, {
+    statusCode: 500,
+    body: '{"error":{"reason":"INTERNAL_SERVER_ERROR","details":"PICKUP_ENDPOINT not defined"}}'
+  })
+})
+
+test('get pin route handler with invalid pickupEndpoint', async t => {
+  process.env.CLUSTER_BASIC_AUTH_TOKEN = 'YES'
+  process.env.INDEXER_ENDPOINT = ''
+  process.env.PICKUP_ENDPOINT = t.context.pickupEndpoint
+
+  const cid = 'QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn'
+  const event = {
+    headers: {
+      authorization: `Basic ${process.env.CLUSTER_BASIC_AUTH_TOKEN}`
+    },
+    pathParameters: {
+      cid
+    }
+  }
+  const response = await handler(event)
+
+  t.deepEqual(response, {
+    statusCode: 500,
+    body: '{"error":{"reason":"INTERNAL_SERVER_ERROR","details":"INDEXER_ENDPOINT not defined"}}'
+  })
+})
+
+test('get pin route handler with result from pickup', async t => {
+  process.env.CLUSTER_BASIC_AUTH_TOKEN = 'YES'
+  process.env.INDEXER_ENDPOINT = t.context.indexerEndpoint
+  process.env.PICKUP_ENDPOINT = t.context.pickupEndpoint
+
+  const cid = 'QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn'
+
+  const nockPickup = nock(t.context.pickupEndpoint)
+  nockPickup
+    .get(`/internal/pins/${cid}`)
+    .reply(200, responseGetPinPinned)
+
+  const event = {
+    headers: {
+      authorization: `Basic ${process.env.CLUSTER_BASIC_AUTH_TOKEN}`
+    },
+    pathParameters: {
+      cid
+    }
+  }
+  const response = await handler(event)
+  t.is(response.statusCode, 200)
+
+  nockPickup.done()
+})
+
+test('get pin route handler with non valid result from pickup', async t => {
+  process.env.CLUSTER_BASIC_AUTH_TOKEN = 'YES'
+  process.env.INDEXER_ENDPOINT = t.context.indexerEndpoint
+  process.env.PICKUP_ENDPOINT = t.context.pickupEndpoint
+
+  const cid = 'QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn'
+
+  const nockIndexer = nock(t.context.indexerEndpoint)
+  nockIndexer
+    .get(`/pins/${cid}`)
+    .reply(200, responseGetPinPinned)
+
+  const nockPickup = nock(t.context.pickupEndpoint)
+  nockPickup
+    .get(`/internal/pins/${cid}`)
+    .reply(500, responseGetPinQueued)
+
+  const event = {
+    headers: {
+      authorization: `Basic ${process.env.CLUSTER_BASIC_AUTH_TOKEN}`
+    },
+    pathParameters: {
+      cid
+    }
+  }
+  const response = await handler(event)
+
+  t.is(response.statusCode, 200)
+  t.is(response.body, JSON.stringify(responseGetPinPinned))
+
+  nockPickup.done()
+  nockIndexer.done()
+})
+
+test('get pin route handler with unpinned result from pickup', async t => {
+  process.env.CLUSTER_BASIC_AUTH_TOKEN = 'YES'
+  process.env.INDEXER_ENDPOINT = t.context.indexerEndpoint
+  process.env.PICKUP_ENDPOINT = t.context.pickupEndpoint
+
+  const cid = 'QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn'
+
+  const nockIndexer = nock(t.context.indexerEndpoint)
+  nockIndexer
+    .get(`/pins/${cid}`)
+    .reply(200, responseGetPinPinned)
+
+  const nockPickup = nock(t.context.pickupEndpoint)
+  nockPickup
+    .get(`/internal/pins/${cid}`)
+    .reply(200, responseGetPinUnpinned)
+
+  const event = {
+    headers: {
+      authorization: `Basic ${process.env.CLUSTER_BASIC_AUTH_TOKEN}`
+    },
+    pathParameters: {
+      cid
+    }
+  }
+  const response = await handler(event)
+
+  t.is(response.statusCode, 200)
+  t.is(response.body, JSON.stringify(responseGetPinPinned))
+
+  nockPickup.done()
+  nockIndexer.done()
+})
