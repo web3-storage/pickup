c8765e26fa0fa86e24a19fac39b29db0d9ece2cb
fabiolrodriguez <fabio.rodriguez@nearform.com>
2023-01-06 10:08:48 -0300

    export logs only on prod and stg

--- stacks/PickupStack.ts
+++ stacks/PickupStack.ts
@@ -38,7 +38,7 @@ export function PickupStack ({ stack }: StackContext): void {
   var labelname = new String(stack);
   labelname = labelname.slice(0, -12)
 
-    if (labelname == "prod-pickup" || labelname == "pr58-pickup") {
+    if (labelname == "prod-pickup" || labelname == "staging-pickup") {
       // add role to read secrets
       service.taskDefinition.taskRole.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMReadOnlyAccess'))
       // configure the custom image to log router
