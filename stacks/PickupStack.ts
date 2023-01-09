19f5c7734d72ab6bd0a381c5968b0a7d2b57036c
Brendt Lucas <brendt.lucas@nearform.com>
2023-01-09 13:57:43 -0500

    Lint fixes

--- stacks/PickupStack.ts
+++ stacks/PickupStack.ts
@@ -1,13 +1,10 @@
 import { StackContext, use, Queue, Bucket } from '@serverless-stack/resources'
 import { BasicApiStack } from './BasicApiStack'
-import { Cluster, ContainerImage, LogDriver, AwsLogDriver, LogDrivers, FireLensLogDriver, Secret, FirelensLogRouter, FirelensLogRouterProps, FirelensLogRouterType, Scope} from 'aws-cdk-lib/aws-ecs'
+import { Cluster, ContainerImage, LogDrivers, Secret, FirelensLogRouterType } from 'aws-cdk-lib/aws-ecs'
 import { Platform } from 'aws-cdk-lib/aws-ecr-assets'
 import { QueueProcessingFargateService } from './lib/queue-processing-fargate-service'
-import { Group, ManagedPolicy } from 'aws-cdk-lib/aws-iam'
-import { servicesVersion } from 'typescript'
-import { urlSource } from 'ipfs/dist/src'
+import { ManagedPolicy } from 'aws-cdk-lib/aws-iam'
 import { aws_ssm } from 'aws-cdk-lib'
-import { aws_secretsmanager, SecretValue } from 'aws-cdk-lib'
 import * as ec2 from 'aws-cdk-lib/aws-ec2'
 
 export function PickupStack ({ app, stack }: StackContext): void {
@@ -45,56 +42,54 @@ export function PickupStack ({ app, stack }: StackContext): void {
     cluster
   })
 
-    if (app.stage == "prod" || app.stage =="staging") {
-      // add role to read parameter
-      service.taskDefinition.taskRole.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMReadOnlyAccess'))
-      // configure the custom image to log router
-      service.taskDefinition.addFirelensLogRouter('log-router',{
-        firelensConfig: {
-          type: FirelensLogRouterType.FLUENTBIT,
+  if (app.stage === 'prod' || app.stage === 'staging') {
+    // add role to read parameter
+    service.taskDefinition.taskRole.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMReadOnlyAccess'))
+    // configure the custom image to log router
+    service.taskDefinition.addFirelensLogRouter('log-router', {
+      firelensConfig: {
+        type: FirelensLogRouterType.FLUENTBIT
+      },
+      image: ContainerImage.fromRegistry('grafana/fluent-bit-plugin-loki:1.6.0-amd64')
+    })
+    // read secret url from parameter store
+    const grafanasecret = aws_ssm.StringParameter.fromStringParameterName(
+      stack,
+      'gf-id',
+      'grafanahost'
+    )
+    // go-ipfs as sidecar!
+    // see: https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_ecs_patterns-readme.html#deploy-application-and-metrics-sidecar
+    service.taskDefinition.addContainer('ipfs', {
+      // route logs to grafana loki
+      logging: LogDrivers.firelens({
+        options: {
+          Name: 'loki',
+          env: app.stage,
+          labels: `{job="${app.stage}-pickup"}`,
+          remove_keys: 'container_id,ecs_task_arn',
+          label_keys: 'container_name,ecs_task_definition,source,ecs_cluster',
+          line_format: 'key_value'
         },
-        image: ContainerImage.fromRegistry('grafana/fluent-bit-plugin-loki:1.6.0-amd64'),
+        secretOptions: { // Retrieved from AWS Systems Manager Parameter Store
+          url: Secret.fromSsmParameter(grafanasecret)
+        }
+      }),
+      image: ContainerImage.fromAsset(new URL('../../pickup/ipfs/', import.meta.url).pathname, {
+        platform: Platform.LINUX_AMD64
       })
-      // read secret url from parameter store
-      const grafanasecret = aws_ssm.StringParameter.fromStringParameterName(
-        stack,
-        'gf-id',
-        'grafanahost',
-      );
-        // go-ipfs as sidecar!
-        // see: https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_ecs_patterns-readme.html#deploy-application-and-metrics-sidecar
-      service.taskDefinition.addContainer('ipfs', {
-        // route logs to grafana loki
-          logging: LogDrivers.firelens({
-            options: {
-              Name: "loki",
-              env: app.stage,
-              labels: "{job=\"" + app.stage + "-pickup\"}",
-              remove_keys: "container_id,ecs_task_arn",
-              label_keys: "container_name,ecs_task_definition,source,ecs_cluster",
-              line_format: "key_value",
-            },
-            secretOptions: { // Retrieved from AWS Systems Manager Parameter Store
-              url: Secret.fromSsmParameter(grafanasecret),
-            },
-          }),
-          image: ContainerImage.fromAsset(new URL('../../pickup/ipfs/', import.meta.url).pathname, {
-            platform: Platform.LINUX_AMD64
-          })
-        })
-
-    } else { 
-       // go-ipfs as sidecar!
-      // see: https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_ecs_patterns-readme.html#deploy-application-and-metrics-sidecar
-      service.taskDefinition.addContainer('ipfs', {
-        // route logs to grafana loki
-          logging: service.logDriver,
-          image: ContainerImage.fromAsset(new URL('../../pickup/ipfs/', import.meta.url).pathname, {
-            platform: Platform.LINUX_AMD64
-          })
-        })
-        
-    }
+    })
+  } else {
+    // go-ipfs as sidecar!
+    // see: https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_ecs_patterns-readme.html#deploy-application-and-metrics-sidecar
+    service.taskDefinition.addContainer('ipfs', {
+      // route logs to grafana loki
+      logging: service.logDriver,
+      image: ContainerImage.fromAsset(new URL('../../pickup/ipfs/', import.meta.url).pathname, {
+        platform: Platform.LINUX_AMD64
+      })
+    })
+  }
 
   basicApi.bucket.cdk.bucket.grantReadWrite(service.taskDefinition.taskRole)
   basicApi.queue.cdk.queue.grantConsumeMessages(service.taskDefinition.taskRole)
