0c140dfb42d8916b7493cd7281ba55511ffaffb0
fabiolrodriguez <fabio.rodriguez@nearform.com>
2023-02-08 17:14:12 -0300

    add healtcheck to const

--- stacks/PickupStack.ts
+++ stacks/PickupStack.ts
@@ -42,6 +42,14 @@ export function PickupStack ({ app, stack }: StackContext): void {
     },
     queue: basicApi.queue.cdk.queue,
     enableExecuteCommand: true,
+    healthCheck: {
+      command: ['CMD-SHELL', 'ipfs cat /ipfs/QmQPeNsJPyVWPFDVHb77w8G42Fvo15z4bG2X8D2GhfbSXc/readme || exit 1'],
+      // the properties below are optional
+      interval: Duration.seconds(5),
+      retries: 2,
+      startPeriod: Duration.seconds(5),
+      timeout: Duration.seconds(20)
+    },
     cluster,
     scalingSteps: [
       { upper: 0, change: -1 },
