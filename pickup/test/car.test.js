f99efffe335a3998b5ca445d7be410813bb22d56
Oli Evans <oli@protocol.ai>
2023-08-15 15:57:00 +0100

    chore: fix car test
    License: MIT
    Signed-off-by: Oli Evans <oli@protocol.ai>

--- pickup/test/car.test.js
+++ pickup/test/car.test.js
@@ -4,11 +4,11 @@ import test from 'ava'
 
 test('checkCar', async t => {
   const { car } = await packToBlob({ input: 'hello world', wrapWithDirectory: false })
-  const { carCid, carBytes, report } = await checkCar(car.stream())
+  const { carCid, carSize, report } = await checkCar(car.stream())
   t.is(carCid.toString(), 'bagbaierao5e6fdcp4p3iyafmbcxudqoe63qcoxegxwpivz5zirw2pulgg4ia')
   t.is(report.blocksIndexed, 1)
   t.is(report.undecodeable, 0)
   t.is(report.uniqueCids, 1)
   t.is(report.structure, 'Complete')
-  t.is(carBytes, 107)
+  t.is(carSize, 107)
 })
